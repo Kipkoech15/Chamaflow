4c6e311e894525917009a57fe32f65dfab8b387c
Kipkoech15 <euginekip@gmail.com>
2025-10-24 14:39:49 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
+  plugins:[react()],
+  build:{
+    outDir:"dist",
     emptyOutDir: true,
     rollupOptions: {
       input: "index.html",
     },
   },
-  resolve: {
-    alias: {
+  resolve:{
+    alias:{
       "@": "/src",
     },
   },
